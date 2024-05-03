7c049929dedce15da99cb05c4d16c7a89dfd5a6f
sheoanh <anhnguyentuan8@gmail.com>
2024-05-03 13:47:48 +0700

    reinitialize project

--- DataProcessingService/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-
-# df = spark \
-#   .readStream \
-#   .format("kafka") \
-#   .option("kafka.bootstrap.servers", "localhost:9092") \
-#   .option("subscribe", "topic1") \
-#   .load()
-# df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
