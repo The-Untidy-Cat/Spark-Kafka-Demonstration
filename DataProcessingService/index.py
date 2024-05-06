f5c0af40131af31265354b979d447e04a3bc6b23
Whitie017 <90831172+Whitie017@users.noreply.github.com>
2024-05-06 21:20:37 +0700

    update query in DataProcessingService index.py

--- DataProcessingService/index.py
+++ DataProcessingService/index.py
@@ -13,32 +13,34 @@ spark = SparkSession \
     .getOrCreate()
 
 df = spark.read.parquet("data/parquet/files")
-df.show()
 
 #Thống kê số lượt truy cập lỗi trong ngày
 def print_error_count_by_days(df):
     today = current_date().cast("date")
     error_count_today = df.filter((col("status_code") >= 400) & (col("datetime").cast("date") == today)).count()
     print(f"Số lượt truy cập lỗi trong ngày hôm nay: {error_count_today}")
-    
+# print_error_count_by_days(df)
+
+#Danh sách các lượt truy cập lỗi trong ngày hôm nay
 def print_list_error_count_by_day(df):
     today = current_date().cast("date")
     error_accesses_today = df.filter((col("status_code") >= 400) & (col("datetime").cast("date") == today))
     print("Danh sách các lượt truy cập lỗi trong ngày hôm nay:")
     error_accesses_today.show(truncate=False)
+# print_list_error_count_by_day(df)
 
+#Danhs sách các trình duyệt truy cập
 def print_list_browser_access(df):
-    browsers = df.select(col("browser")).distinct().rdd.map(lambda row: row[0]).collect()
-    print("Danh sách các trình duyệt đã truy cập:")
-    for browser in browsers:
-        print(f"- {browser}")
+    print("Danh sách các trình duyệt truy cập:")
+    df.show()
+# print_list_browser_access(df)
 
+#Danh sách các lượt truy cập là bot
 def list_isBot(df):
     bot_accesses = df.filter(col("is_bot") == True)
-    bot_accesses_list = bot_accesses.select("device").distinct().rdd.map(lambda row: row[0]).collect()
     print("Danh sách các lượt truy cập không phải từ trình duyệt:")
-    for device in bot_accesses_list:
-        print(f"- {device}")
+    bot_accesses.show(truncate=False)
+# list_isBot(df)
 
 
 
