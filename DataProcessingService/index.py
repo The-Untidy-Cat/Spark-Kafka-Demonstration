68a38c237d165c750ab7924da4d770de1a54379f
Whitie017 <90831172+Whitie017@users.noreply.github.com>
2024-05-06 20:45:02 +0700

    update in DataProcessingService index.py add 4 sql commands

--- DataProcessingService/index.py
+++ DataProcessingService/index.py
@@ -2,6 +2,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StringType, StructField, StructType, LongType, DateType, BooleanType
 from pyspark.sql.functions import from_json
+from pyspark.sql.functions import col, countDistinct, date_trunc, count, current_date
 
 spark = SparkSession \
     .builder \
@@ -13,4 +14,33 @@ spark = SparkSession \
 
 df = spark.read.parquet("data/parquet/files")
 df.show()
+
+#Thống kê số lượt truy cập lỗi trong ngày
+def print_error_count_by_days(df):
+    today = current_date().cast("date")
+    error_count_today = df.filter((col("status_code") >= 400) & (col("datetime").cast("date") == today)).count()
+    print(f"Số lượt truy cập lỗi trong ngày hôm nay: {error_count_today}")
+    
+def print_list_error_count_by_day(df):
+    today = current_date().cast("date")
+    error_accesses_today = df.filter((col("status_code") >= 400) & (col("datetime").cast("date") == today))
+    print("Danh sách các lượt truy cập lỗi trong ngày hôm nay:")
+    error_accesses_today.show(truncate=False)
+
+def print_list_browser_access(df):
+    browsers = df.select(col("browser")).distinct().rdd.map(lambda row: row[0]).collect()
+    print("Danh sách các trình duyệt đã truy cập:")
+    for browser in browsers:
+        print(f"- {browser}")
+
+def list_isBot(df):
+    bot_accesses = df.filter(col("is_bot") == True)
+    bot_accesses_list = bot_accesses.select("device").distinct().rdd.map(lambda row: row[0]).collect()
+    print("Danh sách các lượt truy cập không phải từ trình duyệt:")
+    for device in bot_accesses_list:
+        print(f"- {device}")
+
+
+
+
 
