5417eb46ba3a0a27954e6a7f2ae46eacca85e8dc
Whitie017 <90831172+Whitie017@users.noreply.github.com>
2024-05-07 19:07:07 +0700

    update index.py in dataprocessingservice

--- DataProcessingService/index.py
+++ DataProcessingService/index.py
@@ -19,7 +19,7 @@ def print_error_count_by_days(df):
     today = current_date().cast("date")
     error_count_today = df.filter((col("status_code") >= 400) & (col("datetime").cast("date") == today)).count()
     print(f"Số lượt truy cập lỗi trong ngày hôm nay: {error_count_today}")
-# print_error_count_by_days(df)
+print_error_count_by_days(df)
 
 #Danh sách các lượt truy cập lỗi trong ngày hôm nay
 def print_list_error_count_by_day(df):
@@ -27,20 +27,16 @@ def print_list_error_count_by_day(df):
     error_accesses_today = df.filter((col("status_code") >= 400) & (col("datetime").cast("date") == today))
     print("Danh sách các lượt truy cập lỗi trong ngày hôm nay:")
     error_accesses_today.show(truncate=False)
-# print_list_error_count_by_day(df)
+print_list_error_count_by_day(df)
 
-#Danhs sách các trình duyệt truy cập
-def print_list_browser_access(df):
-    print("Danh sách các trình duyệt truy cập:")
-    df.show()
-# print_list_browser_access(df)
+
 
 #Danh sách các lượt truy cập là bot
 def list_isBot(df):
     bot_accesses = df.filter(col("is_bot") == True)
     print("Danh sách các lượt truy cập không phải từ trình duyệt:")
     bot_accesses.show(truncate=False)
-# list_isBot(df)
+list_isBot(df)
 
 
 
